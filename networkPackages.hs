bf54fed5d219cdbad92105af3d47aa3023a205bd
AntonSexov <antonvedernikov2003@gmail.com>
2024-02-16 15:34:11 +0300

    networkPackages

--- networkPackages.hs
+++ networkPackages.hs
@@ -1,15 +1,36 @@
 module Main (main) where
+import Control.Monad
 
-packageList :: Int -> [Int]
-packageList 0 = []
-packageList n = n : packageList (n - 1)
+processPackages :: Int -> [(Int, Int)] -> [Int]
+processPackages size packets = go [] packets 0
+  where
+    go :: [(Int, Int)] -> [(Int, Int)] -> Int -> [Int]
+    go _ [] _ = []
+    go buffer ((arrival, duration):rest) currentTime =
+      let
+         (buffer', startTimes) = processBuffer buffer arrival currentTime
+         startTime = if length buffer' < size then max currentTime arrival else -1
+      in
+         startTime : go buffer' rest (max currentTime arrival + duration)
+
+    processBuffer :: [(Int, Int)] -> Int -> Int -> ([(Int, Int)], [Int])
+    processBuffer buffer arrival currentTime =
+      let
+         (valid, rest) = span (\(start, _) -> start <= arrival) buffer
+         buffer' = drop (length valid) rest ++ [(currentTime + duration, currentTime + duration) | (_, duration) <- valid]
+      in
+         (buffer', map fst valid)
+
 
 main :: IO ()
 main = do
      input <- getLine
-     let inputArr = map read(words input) :: [Int]
-     let (size:nPackets:_) = inputArr
-     let test = packageList(nPackets)
+     let (size:nPackets:_) = map read $ words input :: [Int]
 
-     putStrLn $ show size ++ " " ++ show nPackets ++ "\n array: " ++ show test
-     
+     packets <- replicateM nPackets $ do
+        line <- getLine
+        let (arrivalTime:processingTime:_) = map read $ words line :: [Int]
+        return (arrivalTime, processingTime)
+
+     print (processPackages size packets)
+
