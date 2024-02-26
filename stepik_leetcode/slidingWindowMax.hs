4055b6d2997ca09796fc3905c1baf56e51db05f7
AntonSexov <antonvedernikov2003@gmail.com>
2024-02-26 17:44:43 +0300

    slidingWindow

--- stepik_leetcode/slidingWindowMax.hs
+++ stepik_leetcode/slidingWindowMax.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE GADTs #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use unwords" #-}
 module Main (main) where
+import Data.List (intercalate)
 
 newtype Deque where
   Deque :: [Int] -> Deque
   deriving Show
 
 popRight :: Deque -> Deque
-popRight (Deque []) = error "can't pop empty queue" 
+popRight (Deque []) = error "can't pop empty queue"
 popRight (Deque x) = Deque (init x)
 
 popLeft :: Deque -> Deque
@@ -21,23 +25,23 @@ pushRight x (Deque y) = Deque (reverse (x:reverse y))
 empty :: Deque
 empty = Deque []
 
-slidingWindowMax :: Int -> [Int] -> Int -> [Int]
-slidingWindowMax n arr m
-    | n * m == 0 = []
-    | m == 1 = arr
-    | otherwise = findMax (n - m + 1) arr m []
-    where
-        findMax :: Int -> [Int] -> Int -> [Int] -> [Int]
-        findMax 0 arr m max' = max'
-        findMax n arr m max' = [0]
+processWindow :: Deque -> Int -> Int
+processWindow (Deque []) _ = error "empty queue"
+processWindow (Deque q) m = maximum (take m q)
 
+slidingWindowMax :: Deque -> Int -> Int -> [Int]
+slidingWindowMax q n m
+  | n < m = error "window larger than queue"
+  | otherwise = map (\i -> processWindow (Deque (drop i (unwrap q))) m) [0..n-m]
+  where
+    unwrap (Deque xs) = xs
 
 main :: IO()
 main = do
     n <- getLine
     let arrSize = read n :: Int
     arrInput <- getLine
-    let arr = map read (words arrInput) :: [Int]
+    let arr = Deque (map read (words arrInput) :: [Int])
     m <- getLine
     let windowSize = read m :: Int
-    print $ slidingWindowMax arrSize arr windowSize
+    putStrLn $ intercalate " " $ show <$> slidingWindowMax arr arrSize windowSize
