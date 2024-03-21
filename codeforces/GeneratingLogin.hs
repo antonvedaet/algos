1b54842819e30467347cdb432560e373e0c51c16
AntonSexov <antonvedernikov2003@gmail.com>
2024-03-21 20:38:31 +0300

    better version

--- codeforces/GeneratingLogin.hs
+++ codeforces/GeneratingLogin.hs
@@ -1,11 +1,15 @@
 module Main(main) where
 
-computeLogin :: String -> String -> String
-computeLogin (x:xs) (y:ys) = go xs y [x]
-    where
-        go :: String -> Char -> String -> String
-        go [] l r = r ++ [l]
-        go (x:xs) l r = if x >= l then go [] l r else go xs l (r ++ [x])
+-- computeLogin :: String -> String -> String
+-- computeLogin (x:xs) (y:ys) = go xs y [x]
+--     where
+--         go :: String -> Char -> String -> String
+--         go [] l r = r ++ [l]
+--         go (x:xs) l r = if x >= l then go [] l r else go xs l (r ++ [x])
+
+computeLoginBetter :: String -> String -> String
+computeLoginBetter (x:xs) (y:_) = x : takeWhile (< y) xs ++ [y]  
+
 
 main :: IO()
 main = do
@@ -13,4 +17,4 @@ main = do
     let (fname:lastname:_) = words inp :: [String]
 
 
-    putStrLn $ computeLogin fname lastname
+    putStrLn $ computeLoginBetter fname lastname
